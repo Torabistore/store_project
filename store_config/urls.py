2ca723ad843b5cbe0a8852ab2ac74c9d4b753af6
torabistore <ti.ragestream@gmail.com>
2025-07-07 18:24:38 +0330

    🔧 افزودن فایل‌های static جدید و اصلاح مسیر فایل‌های استاتیک در urls.py

--- store_config/urls.py
+++ store_config/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # مسیرهای اپلیکیشن catalog
-    path('', include('catalog.urls')),  
-
-    # مسیرهای اپلیکیشن accounts
-    path('accounts/', include('accounts.urls')),  
+    path('', include('catalog.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
+
+# اضافه کردن مسیرهای static و media خارج از لیست اصلی
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
