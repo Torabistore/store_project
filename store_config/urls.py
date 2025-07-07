736d8c3138b70e14435928ac5e0a8863ae84cd5a
torabistore <ti.ragestream@gmail.com>
2025-07-07 11:09:18 +0330

    Fix urls.py in store_config

--- store_config/urls.py
+++ store_config/urls.py
@@ -1,15 +1,7 @@
-from django.urls import path
-from . import views
-
-app_name = 'catalog'
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('products/', views.product_list, name='product_list'),
-    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
-    path('about/', views.about_page, name='about_page'),
-    path('contact/', views.contact_page, name='contact_page'),
-    
-    # سبد خرید
-    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls')),  # همه مسیرهای catalog از اینجا وارد میشن
 ]
