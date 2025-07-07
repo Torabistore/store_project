cc19776085f8154def7dfa8ea46a93c8e37d854d
torabistore <ti.ragestream@gmail.com>
2025-07-07 10:23:14 +0330

    Update styles and templates

--- store_config/settings.py
+++ store_config/settings.py
@@ -93,6 +93,8 @@ USE_TZ = True
 # فایل‌های استاتیک و مدیا
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
