0aeb2de1eb4ded40051e40b6df24a3e0f2b8611f
torabistore <ti.ragestream@gmail.com>
2025-07-16 18:38:41 +0330

    افزودن مدیریت PaymentRequest در پنل ادمین

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Product, ProductImage, Order, OrderItem
+from .models import PaymentRequest
 
 # ✅ محصول
 class ProductImageInline(admin.TabularInline):
@@ -33,3 +34,12 @@ class OrderItemAdmin(admin.ModelAdmin):
     list_display = ['order', 'product', 'quantity', 'price']
     list_filter = ['order']
     search_fields = ['product__name']
+
+
+
+@admin.register(PaymentRequest)
+class PaymentRequestAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'status', 'created_at', 'reference_number']
+    list_filter = ['status', 'created_at']
+    search_fields = ['user__username', 'reference_number']
+    readonly_fields = ['payment_receipt']
